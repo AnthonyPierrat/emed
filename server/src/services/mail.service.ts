6c71fa4f9e25572c9e996968f2fae302b0c6b61c
remyguillaumot <guillaumotremy@gmail.com>
2019-06-20 09:56:20 +0200

    merge conflict

--- server/src/services/mail.service.ts
+++ server/src/services/mail.service.ts
@@ -1,12 +1,15 @@
+import User, { UserModel } from "../models/user.model";
+
 const nodeMailer = require("nodemailer");
 const smtpTransport = require("nodemailer-smtp-transport");
-var fs = require('fs');
+import * as ejs from "ejs";
+import {readFileSync} from "fs";
 
 export default class MailService {
 
     constructor() { }
 
-    public async sendMail(email: string, publicKey: any) {
+    public async sendMail(user: User) {
 
         const transporter = nodeMailer.createTransport(smtpTransport({
             service: "gmail",
@@ -16,14 +19,33 @@ export default class MailService {
             }
         }));
 
-        //  var text = ejs.render('Hello, <%= firstName %> <%= lastName %>!', user);
-        var template = fs.readFileSync('mail.html',{encoding:'utf-8'});
-
         const mailOptions = {
-            from: '"Emed" <xx@gmail.com>', // sender address
-            to: email, // list of receivers
-            subject: "Bienvenue chez E-med", 
-            html : template
+            from: '"noreply-emed" <noreply-emed@gmail.com>', // sender address
+            to: user.email, // list of receivers
+            subject: "Bienvenue chez E-med ",
+            html : ejs.render(
+                readFileSync(`${__dirname}/../template/mail.template.ejs`).toString(),
+                    {
+                        publicKey: user.publicKey
+                    })
+
+            /*`<!doctype html>
+                        <html lang="en">
+                            <head>
+                            <header>
+                                <h3>Vous venez de rejoindre E-med</h3>
+                            </header>
+                            <body>
+                                Bonjour ` + user.publicKey + `,<br>
+                                Vous venez de rejoindre la solution décentralisée E-med.<br>
+                                Vous pouvez désormais partarger de manière sécurisée vos informations médicales aux personnes de votre choix en partageant votre identifiant unique.<br>
+                                Votre clef de sécurité est la suivante : <b>` + user.publicKey + `<b><br>
+                                Nous vous remercions pour votre confiance<br>
+                                <br>
+                                <i>L'équipe E-med<i>
+                            </body>
+                            <userKey></userKey>
+                        </html>`*/
         };
 
         transporter.sendMail(mailOptions, (error: any, info: any) => {
